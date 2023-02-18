c2ca4762df7d5a6e16677d74bf0115ab5efcce50
tyriongump <tyriongump@gmail.com>
2023-02-19 00:06:17 +1100

    leetcode-longest-common-prefix

---
+++ 14.longest-common-prefix.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode id=14 lang=java
+ *
+ * [14] Longest Common Prefix
+ */
+
+// @lc code=start
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String s1 = strs[0];
+        String s2 = strs[strs.length-1];
+        int idx = 0;
+        while(idx < s1.length() && idx < s2.length()){
+            if(s1.charAt(idx) == s2.charAt(idx)){
+                idx++;
+            } else {
+                break;
+            }
+        }
+        return s1.substring(0, idx);
+    }
+}
+// @lc code=end
+
