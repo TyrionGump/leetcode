6b0bee9e32a0a28845b23700ab461040aaac769e
tyriongump <tyriongump@gmail.com>
2023-02-20 00:29:05 +1100

    leetcode-valid-parentheses

--- 20.valid-parentheses.java
+++ 20.valid-parentheses.java
@@ -1,5 +1,8 @@
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Deque;
+import java.util.Map;
 
 /*
  * @lc app=leetcode id=20 lang=java
@@ -10,16 +13,23 @@ import java.util.Deque;
 // @lc code=start
 class Solution {
     public boolean isValid(String s) {
-        Map<Char, Char> rightToLeftBrackets = Map.of('}', '{', ']', '[', ')', '(');
+        Map<Character, Character> rightToLeftBrackets = Map.of('}', '{', ']', '[', ')', '(');
 
-        Set<Char> rightBrackets = rightToLeftBrackets.keyS
+        Set<Character> rightBrackets = rightToLeftBrackets.keySet();
+        List<Character> leftBrackets = new ArrayList<>(rightToLeftBrackets.values());
 
         Deque stack = new ArrayDeque<>();
 
-        for (Char c : s.toCharArray()) {
-            if 
+        for (Character c : s.toCharArray()) {
+
+            if (leftBrackets.contains(c)) {
+                stack.add(c);
+            } else if (stack.isEmpty() || !stack.pollLast().equals(rightToLeftBrackets.get(c))) {
+                return false;
+            }
         }
+
+        return stack.isEmpty();
     }
 }
 // @lc code=end
-
