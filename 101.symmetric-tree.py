64b2ddf683e6ebde45e8c3a525d0302313c88c76
tyriongump <tyriongump@gmail.com>
2023-03-23 23:57:30 +1100

    leetcode-symmetric-tree

---
+++ 101.symmetric-tree.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=101 lang=python3
+#
+# [101] Symmetric Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return self.isSymmetric(root.left).val == self.isSymmetric(root.right).val
+
+        
+# @lc code=end
+
