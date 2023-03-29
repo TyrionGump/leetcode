8d5c9a3c8db05296365b0babe30d608fac5e6b0a
tyriongump <tyriongump@gmail.com>
2023-03-30 00:01:55 +1100

    leetcode-balanced-binary-tree-java

---
+++ 110.balanced-binary-tree.java
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode id=110 lang=java
+ *
+ * [110] Balanced Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return getHeight(root) != -1;
+    }
+
+    public int getHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftHeight = getHeight(node.left);
+
+        if (leftHeight == -1) {
+            return -1;
+        }
+
+        int rightHeight = getHeight(node.right);
+
+        if (rightHeight == -1) {
+            return -1;
+        }
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
+// @lc code=end
+
