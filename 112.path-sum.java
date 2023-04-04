8dd9ab8aa4da1e5d1b3b889f394147c3a6cd10ef
tyriongump <tyriongump@gmail.com>
2023-04-04 23:58:26 +1000

    leetcode-path-sum

---
+++ 112.path-sum.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=112 lang=java
+ *
+ * [112] Path Sum
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.val == targetSum && root.left == null && root.right == null) {
+            return true;
+        }
+
+        return this.hasPathSum(root.left, targetSum - root.val) || this.hasPathSum(root.right, targetSum - root.val);
+    }
+}
+// @lc code=end
+
