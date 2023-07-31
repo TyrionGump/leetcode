9e14fa003bfc836c73a99d36d674a13c91898215
tyriongump <tyriongump@gmail.com>
2023-08-01 09:06:28 +1000

    binary-tree-postorder-traversal-java

---
+++ 145.binary-tree-postorder-traversal.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=145 lang=java
+ *
+ * [145] Binary Tree Postorder Traversal
+ */
+
+// @lc code=start
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        this.traversal(root, res);
+        return res;
+    }
+
+    public void traversal(TreeNode root, List<Integer> res) {
+        if (root != null) {
+            traversal(root.left, res);
+            traversal(root.right, res);
+            res.add(root.val);
+        }
+    }
+}
+// @lc code=end
+
