4c23bf19077a7429ad27d48f3807f750fba4b2ad
tyriongump <tyriongump@gmail.com>
2023-08-07 23:59:05 +1000

    reverse-bits-python-java

---
+++ 190.reverse-bits.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=190 lang=java
+ *
+ * [190] Reverse Bits
+ */
+
+// @lc code=start
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int res = 0;
+
+        for (int i = 0; i < 32; i++) {
+            res <<= 1;
+            res += n & 1;
+            n >>= 1;
+        }
+
+        return res;
+    }
+}
+// @lc code=end
+
